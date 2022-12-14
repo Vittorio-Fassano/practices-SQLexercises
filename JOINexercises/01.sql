936a23ef77360ce87cb747bdeba35ed730e8fd4b
Vittorio <vittfassano@gmail.com>
2022-12-14 20:06:11 -0300

    JOINexercises - 01 (refact)

--- JOINexercises/01.sql
+++ JOINexercises/01.sql
@@ -1,1 +1,4 @@
-SELECT users.* FROM users JOIN cities ON users."cityId" = cities.id WHERE "cityId" = 3658;
+SELECT users.id AS id, users.name AS name, cities.name AS city 
+FROM users 
+JOIN cities ON users."cityId" = cities.id 
+WHERE cities.name = 'Rio de Janeiro';
