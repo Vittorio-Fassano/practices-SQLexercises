ed66330039eb6ae16616d43d71eeb31d36ff9bae
Vittorio <vittfassano@gmail.com>
2022-12-19 10:31:36 -0300

    modelagemPractice orders_product

--- MODELAGEMpratice/modelagem.sql
+++ MODELAGEMpratice/modelagem.sql
@@ -8,16 +8,16 @@ CREATE TABLE "cities" (
 
 CREATE TABLE "adresses" (
     "id" SERIAL PRIMARY KEY,
-    "street" TEXT NOT NULL, 
-    "number" INTEGER NOT NULL,  
+    "street" TEXT NOT NULL,
+    "number" INTEGER NOT NULL,
     "city_id" INTEGER NOT NULL REFERENCES "cities"("id")
 );
 
 CREATE TABLE "users" (
     "id" SERIAL PRIMARY KEY,
-    "name" TEXT NOT NULL, 
-    "email" TEXT NOT NULL UNIQUE,  
-    "password" TEXT NOT NULL,  
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL UNIQUE,
+    "password" TEXT NOT NULL,
     "adress_id" INTEGER NOT NULL REFERENCES "adresses"("id")
 );
 
@@ -39,8 +39,8 @@ CREATE TABLE "images" (
 
 CREATE TABLE "products" (
     "id" SERIAL PRIMARY KEY,
-    "name" TEXT NOT NULL, 
-    "price" INTEGER NOT NULL,  
+    "name" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
     "size_id" INTEGER NOT NULL REFERENCES "sizes"("id"),
     "category_id" INTEGER NOT NULL REFERENCES "categories"("id"),
     "image_id" INTEGER NOT NULL REFERENCES "images"("id")
@@ -54,10 +54,16 @@ CREATE TABLE "orders_state" (
 
 CREATE TABLE "orders" (
     "id" SERIAL PRIMARY KEY,
-    "date" DATE NOT NULL DEFAULT NOW(),  
+    "date" DATE NOT NULL DEFAULT NOW(),
     "user_id" INTEGER NOT NULL REFERENCES "users"("id"),
     "adress_id" INTEGER NOT NULL REFERENCES "adresses"("id"),
     "state_id" INTEGER NOT NULL REFERENCES "orders_state"("id")
 );
 
-
+CREATE TABLE "orders_product" (
+    "id" SERIAL PRIMARY KEY,
+    "price_product" INTEGER NOT NULL,
+    "purchases" INTEGER NOT NULL DEFAULT 0,
+    "product_id" INTEGER NOT NULL REFERENCES "products"("id"),
+    "order_id" INTEGER NOT NULL REFERENCES "orders"("id")
+);
